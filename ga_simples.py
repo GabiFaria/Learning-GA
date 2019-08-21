25af02ad59602b89be704df24d9e9557ed0756df
Gabriela Faria <gabrielafariaoliveira@gmail.com>
2019-08-20 21:44:24 -0300

    GabiFaria

--- ga_simples.py
+++ ga_simples.py
@@ -2,45 +2,49 @@ import random
 from random import choices
 
 class Mae_Natureza:
-	def InicializaCromossomo (tamanhogene):
+	
+	def InicializaCromossomo (self,tamanhogene):
 		i = 0
 		genes = []
 		for i in range(tamanhogene):
-			if (random.random()<0.5):
+			if (random.random()<0.7):
 				genes.append(0)
 			else:
 				genes.append(1)
 		return (genes)
+	
 	def InicializaPopulacao (self,tamanhopop,tamanhogene):
 		i = 0 
-		cromossomos =  []
+		cromossomos =  Mae_Natureza()
+		cromo = []
 		for i in range(tamanhopop):
-			cromossomos.append(self.InicializaCromossomo(tamanhogene))
+			cromo.append(cromossomos.InicializaCromossomo(tamanhogene))
 
-		return cromossomos
-
-	def Avaliacao (populacao,tamanhopop,tamanhogene):
+		return cromo
+	
+	def Avaliacao (self,pop,tamanhopop,tamanhogene):
 		avaliacoes = []
 		aux =0 
 		k= tamanhogene-1
 		for i in range(tamanhopop):
 			for j in range(tamanhogene):
-				aux = aux + populacao[i][j]*(2**k)
+				aux = aux + pop[0][i][j]*(2**k)
 				k = k-1
 			avaliacoes.append(aux**2)
 			aux = 0
 			k = tamanhogene-1
 		return avaliacoes
-
-	def Selecao (populacao, avaliacao):
+	
+	def Selecao (self,pop,avaliacao):
+	
 		probabilidade = []
 		pais = []
 		for i in range(len(avaliacao)):
-			probabilidade.append((avaliacao[i]/sum(avaliacao))*100)
-		pais.append(choices(populacao,probabilidade,k=6))
+			probabilidade.append((avaliacao[0][i]/sum(avaliacao[0]))*100)
+		pais.append(choices(pop,probabilidade,k=6))
 		return pais			  
-
-	def Crossover (pais,tamanhogene):
+	
+	def Crossover (self,pais,tamanhogene):
 		point = random.randint(0,tamanhogene-2)
 		filho1 = []
 		filho2 = []
@@ -53,15 +57,25 @@ class Mae_Natureza:
 
 
 class Evolucao():
-	def evoluindo(tamanhopop,tamanhogene):
-		for i in range(4):
-			populacao = []
-			avaliacoes = []
-			populacao.append(Mae_Natureza.InicializaPopulacao(Mae_Natureza.InicializaCromossomo,tamanhopop,tamanhogene))
-			avaliacoes.append(Mae_Natureza.Avaliacao(populacao,tamanhopop,tamanhogene))
-			pais = (Mae_Natureza.Selecao(populacao,avaliacoes))
-			populacao.clear()
-			populacao = Mae_Natureza.Crossover(pais,tamanhogene)
-			print ("Somátorio das avaliacões da %dº População:",i)
+	
+	def evoluindo(self,tamanhopop,tamanhogene):
+		maximo = 0
+		i = 0
+		while (maximo<2550):
+			populacao = Mae_Natureza()
+			pais = []
+			pop = []
+			aval = []
+			pop.append(populacao.InicializaPopulacao(tamanhopop,tamanhogene))
+			aval.append(populacao.Avaliacao(pop,tamanhopop,tamanhogene))
+			pais.append(populacao.Selecao(pop,aval))
+			pop.clear()
+			pop.append(populacao.Crossover(pais,tamanhogene))
+			maximo = sum(aval[0])
+			i+=1
+			print ("Somátorio das avaliacões da ",i,"º População:",maximo)
 
-Evolucao.evoluindo(10,4)
+e = Evolucao() 
+tamanhopop = 10
+tamanhogene = 4
+e.evoluindo(tamanhopop,tamanhogene)
